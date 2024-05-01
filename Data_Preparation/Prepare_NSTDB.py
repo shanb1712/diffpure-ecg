bd85539df608460e00f70d5167ed2c450189d03e
shanb1712 <shanybiton@gmail.com>
2024-05-01 14:19:52 +0300

    data preparation for running diffusion model

---
+++ Data_Preparation/Prepare_NSTDB.py
@@ -0,0 +1,50 @@
+import wfdb
+import _pickle as pickle
+from scipy.signal import resample_poly
+import math
+
+
+def resample_noise(signal, orig_fs, actual_fs):
+    L = math.ceil(len(signal) * actual_fs / orig_fs)
+    # Padding data to avoid edge effects caused by resample
+    normBeat = list(reversed(signal)) + list(signal) + list(reversed(signal))
+
+    # resample
+    res = resample_poly(normBeat, actual_fs, orig_fs)
+    res = res[L - 1:2 * L - 1]
+
+    return res
+
+
+def prepare(path_to_data):
+    bw_signals, bw_fields = wfdb.rdsamp(path_to_data / 'mit-bih-noise-stress-test-database-1.0.0/' / 'bw')
+    em_signals, em_fields = wfdb.rdsamp(path_to_data / 'mit-bih-noise-stress-test-database-1.0.0/' / 'em')
+    ma_signals, ma_fields = wfdb.rdsamp(path_to_data / 'mit-bih-noise-stress-test-database-1.0.0/' / 'ma')
+
+    orig_fs = 360
+    actual_fs = 400
+
+    bw_signals_re = resample_noise(bw_signals, orig_fs, actual_fs)
+    em_signals_re = resample_noise(em_signals, orig_fs, actual_fs)
+    ma_signals_re = resample_noise(ma_signals, orig_fs, actual_fs)
+
+    for key in bw_fields:
+        bw_fields['fs'] = actual_fs
+        bw_fields['sig_len'] = len(bw_signals_re)
+        print(key, bw_fields[key])
+
+    for key in em_fields:
+        em_fields['fs'] = actual_fs
+        em_fields['sig_len'] = len(em_signals_re)
+        print(key, em_fields[key])
+
+    for key in ma_fields:
+        ma_fields['fs'] = actual_fs
+        ma_fields['sig_len'] = len(ma_signals_re)
+        print(key, ma_fields[key])
+
+    # Save Data
+    with open(path_to_data / 'NoiseBWL.pkl', 'wb') as output:  # Overwrites any existing file.
+        pickle.dump([bw_signals_re, em_signals_re, ma_signals_re], output)
+    print('=========================================================')
+    print('MIT BIH data noise stress test database (NSTDB) saved as pickle')
---
+++ Data_Preparation/data_preparation.py
@@ -0,0 +1,83 @@
+import numpy as np
+import _pickle as pickle
+import pathlib
+import h5py
+import pandas as pd
+# from Data_Preparation import Prepare_NSTDB
+
+
+def get_data(y, traces_ids):
+    # ----- Data settings ----- #
+    diagnosis = ["1dAVb", "RBBB", "LBBB", "SB", "AF", "ST"]
+    # ------------------------- #
+    y.set_index('id_exam', drop=True, inplace=True)
+    y = y.reindex(traces_ids, copy=False)
+    df_diagnosis = y.reindex(columns=[d for d in diagnosis])
+    y = df_diagnosis.values
+    return y
+
+def Load_Data(path_to_hdf5, path_to_csv, portion=None):
+    # Get tracings
+    f = h5py.File(path_to_hdf5 / 'traces.hdf5', "r")
+    x = f['signal']
+
+    traces_ids = np.array(f['id_exam'])[:portion]
+
+    # Get annotations
+    y_csv = pd.read_csv(path_to_csv / 'annotations.csv')
+    y = get_data(y_csv, traces_ids)
+    # Get ids that are used for training the classifier
+    idx = np.arange(len(traces_ids))
+    partition = {'validation': idx[-round(0.02 * len(idx)):],
+                 'train': idx[:len(idx) - round(0.02 * len(idx))]}
+    idx_val = np.sort(np.asarray(partition['validation']))
+    idx_train = np.sort(np.asarray(partition['train']))
+    return x, y, idx_train, idx_val
+
+
+def Load_Noise(noise_test_len=0, noise_version=1):
+    path_to_data = pathlib.PurePath('./data/')
+    print('Getting the Data ready ... ')
+
+    # The seed is used to ensure the ECG always have the same contamination level
+    # this enhance reproducibility
+    seed = 1234
+    np.random.seed(seed=seed)
+    # Prepare_NSTDB.prepare(path_to_data)
+
+    # Load NSTDB
+    with open(path_to_data / 'NoiseBWL.pkl', 'rb') as input:
+        nstdb = pickle.load(input)
+
+    #####################################
+    # NSTDB
+    #####################################
+
+    [bw_signals, _, _] = nstdb
+    # [_, em_signals, _ ] = nstdb
+    # [_, _, ma_signals] = nstdb
+    bw_signals = np.array(bw_signals)
+
+    bw_noise_channel1_a = bw_signals[0:int(bw_signals.shape[0] / 2), 0]
+    bw_noise_channel1_b = bw_signals[int(bw_signals.shape[0] / 2):-1, 0]
+    bw_noise_channel2_a = bw_signals[0:int(bw_signals.shape[0] / 2), 1]
+    bw_noise_channel2_b = bw_signals[int(bw_signals.shape[0] / 2):-1, 1]
+
+    #####################################
+    # Data split
+    #####################################
+    if noise_version == 1:
+        noise_test = bw_noise_channel2_b
+        noise_train = bw_noise_channel1_a
+    elif noise_version == 2:
+        noise_test = bw_noise_channel1_b
+        noise_train = bw_noise_channel2_a
+    else:
+        raise Exception("Sorry, noise_version should be 1 or 2")
+    if noise_test_len> 0:
+        rnd_test = np.random.randint(low=20, high=200, size=noise_test_len) / 100
+        # Saving the random array so we can use it on the amplitude segmentation tables
+        np.save('rnd_test.npy', rnd_test)
+        print('rnd_test shape: ' + str(rnd_test.shape))
+
+    return noise_train, noise_test
